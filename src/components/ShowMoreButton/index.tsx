b4bf979fd7248f99efba3e285d6ca5a3049c94d6
Maxim Lepekha <m.lepekha@yandex.ru>
2021-10-08 01:38:44 +0300

    ShowMoreButton component

---
+++ src/components/ShowMoreButton/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './index.css';
+
+export const ShowMoreButton = () => {
+  return (
+    <button className="ShowMoreButton">Показать еще 5 билетов!</button>
+  );
+};
--- src/components/Tickets/index.tsx
+++ src/components/Tickets/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Ticket as TicketType } from '../../entities/ticket';
 import { Ticket } from '../Ticket';
 import './index.css';
+import { ShowMoreButton } from '../ShowMoreButton';
 
 type Params = {
   tickets: TicketType[],
@@ -10,11 +11,16 @@ type Params = {
 export const Tickets = ({ tickets }: Params) => {
   return (
     <div className="Tickets">
-      {tickets.map((ticket, index) => (
-        <div key={index} className="Tickets__ticket">
-          <Ticket ticket={ticket} />
-        </div>
-      ))}
+      <div className="Tickets__list">
+        {tickets.map((ticket, index) => (
+          <div key={index} className="Tickets__ticket">
+            <Ticket ticket={ticket} />
+          </div>
+        ))}
+      </div>
+      <div className="Tickets__showMoreButton">
+        <ShowMoreButton />
+      </div>
     </div>
   );
 };
