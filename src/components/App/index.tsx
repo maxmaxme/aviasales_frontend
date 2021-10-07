2b9063c46747aef3e1ab67f9356731bd1cf82fc5
Maxim Lepekha <m.lepekha@yandex.ru>
2021-10-08 01:33:10 +0300

    Header component

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Tickets } from './components/Tickets';
-import { Ticket } from './entities/ticket';
-import ApiResponse from './tickets.json';
-import './App.css';
-import { Filter } from './components/Filter';
-import { Tabs } from './components/Tabs';
+import { Tickets } from '../Tickets';
+import { Filter } from '../Filter';
+import { Tabs } from '../Tabs';
+import { Header } from '../Header';
+import { Ticket } from '../../entities/ticket';
+import ApiResponse from '../../tickets.json';
+import './index.css';
 
 export const App = () => {
   const tickets: Ticket[] = ApiResponse.tickets.sort(() => .5 - Math.random()).slice(0, 10);
   return (
     <div className="App">
+      <Header />
       <div className="App__content">
         <Filter />
         <div>
