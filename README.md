a64421e05a6df02b29a6f14e1e3daafea88f6d09
Maxim Lepekha <m.lepekha@yandex.ru>
2021-10-08 03:07:47 +0300

    sort tickets

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Aviasales test task
 
-https://github.com/KosyanMedia/test-tasks/tree/master/aviasales_frontend
+Task: https://github.com/KosyanMedia/test-tasks/tree/master/aviasales_frontend
+
+Demo: https://maxmaxme.github.io/aviasales_frontend/
--- src/components/Tickets/index.tsx
+++ src/components/Tickets/index.tsx
@@ -3,9 +3,10 @@ import { Ticket } from '../Ticket';
 import './index.css';
 import { ShowMoreButton } from '../ShowMoreButton';
 import { AppContext } from '../../context';
+import { TAB_IDS } from '../../entities/tab';
 
 export const Tickets = () => {
-  const { state: { tickets, filters } } = useContext(AppContext);
+  const { state: { tickets, filters, sort } } = useContext(AppContext);
   const filteredTickets = tickets.filter((ticket) => {
     if (filters.stopsCount.length) {
       const ticketStopsCount = ticket.segments
@@ -16,10 +17,31 @@ export const Tickets = () => {
 
     return true;
   });
+
+  const sortedTickets = filteredTickets.sort((a, b): number => {
+    switch (sort) {
+    case TAB_IDS.FAST:
+      const timeA = a.segments
+        .map((segment) => segment.duration)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+      const timeB = b.segments
+        .map((segment) => segment.duration)
+        .reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+
+      return timeA - timeB;
+
+    case TAB_IDS.CHEAP:
+      return a.price - b.price;
+
+    case TAB_IDS.OPTIMAL: // Считаем, что бекенд по умолчанию возвращает оптимальную сортировку ;)
+    default:
+      return 1;
+    }
+  });
   return (
     <div className="Tickets">
       <div className="Tickets__list">
-        {filteredTickets.slice(0, 5).map((ticket, index) => (
+        {sortedTickets.slice(0, 5).map((ticket, index) => (
           <div key={index} className="Tickets__ticket">
             <Ticket ticket={ticket} />
           </div>
